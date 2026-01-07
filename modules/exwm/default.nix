38ac562ec99c596915e47eb08dbce2d029924791
Draune <sioul.duaner@gmail.com>
2026-01-07 17:05:03 +0100

    wallpaper + opacity

--- modules/exwm/default.nix
+++ modules/exwm/default.nix
@@ -2,6 +2,7 @@
   # Don't really needs to install Xephyr (xorg.xserver) since I will already have Xorg installed
   
   home.packages = with pkgs; [
+    feh
     (writeShellScriptBin "xephyr-exwm" (builtins.readFile ./xephyr-exwm/xephyr-exwm))
   ];
 
@@ -15,5 +16,11 @@
   };
 
   home.file.".xinitrc".source = ./xinitrc;
-  
+  home.file.".wallpaper.jpg".source = ./wallpaper.jpg;
+
+  services.picom = {
+    enable = true;
+    activeOpacity = 0.8;
+    inactiveOpacity = 0.8;
+  };
 }
