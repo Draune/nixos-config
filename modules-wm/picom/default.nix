cf9bd7511cf736c13b8fbee6c9a780b50119a16e
Draune <sioul.duaner@gmail.com>
2026-01-12 21:15:11 +0100

    update picom opacity

--- modules-wm/picom/default.nix
+++ modules-wm/picom/default.nix
@@ -1,7 +1,12 @@
 {pkgs, config, ...}: {
   services.picom = {
     enable = true;
-    activeOpacity = 0.92;
-    inactiveOpacity = 0.92;
+    activeOpacity = 0.90;
+    inactiveOpacity = 0.90;
+    opacityRules = [
+      "80:class_g = 'firefox'"
+      "80:class_g = 'vesktop'"
+      "70:class_g = 'kitty'"
+    ];
   };
 }
